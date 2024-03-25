bed388011de98706aaa7ff053253a2d01777630a
Evangeline <evangeline.klimpsch@gmail.com>
2024-03-25 15:36:27 -0700

    Test API connection

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log(fetch('https://openlibrary.org/search.json'))
+fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+    .then(response => console.log(response))
+    .catch(error => console.error(error))
